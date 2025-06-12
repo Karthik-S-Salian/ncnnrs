cc8c1565a9bd35f3cd7a205c054c958cec0ec1d8
Karthik-S-Salian <karthikssalian5@gmail.com>
2025-06-12 23:26:33 +0530

    fix:could not find bin files

--- build.rs
+++ build.rs
@@ -3,32 +3,58 @@ use std::path::PathBuf;
 
 fn main() {
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let ncnn_include_dir = env::var("NCNN_INCLUDE_DIR")
-        .map(|dir| PathBuf::from(dir))
-        .expect(
-            "ERROR: please set NCNN_INCLUDE_DIR,e.g. export NCNN_INCLUDE_DIR=/path/to/ncnn/include",
-        );
-    if !ncnn_include_dir.join("c_api.h").exists() {
+
+    let ncnn_dir = env::var("NCNN_DIR")
+        .expect("ERROR: please set NCNN_DIR (e.g. export NCNN_DIR=/path/to/ncnn)");
+    let ncnn_path = PathBuf::from(&ncnn_dir);
+
+    let include_path = ncnn_path.join("include/ncnn");
+    let lib_path = ncnn_path.join("lib");
+
+    let c_api_path = include_path.join("c_api.h");
+    if !c_api_path.exists() {
         panic!(
-            "ERROR: please set NCNN_INCLUDE_DIR,e.g2. export NCNN_INCLUDE_DIR=/path/to/ncnn/include"
+            "ERROR: Missing c_api.h at {}. Make sure NCNN_DIR is set to the root of your NCNN build.",
+            c_api_path.display()
         );
     }
 
-    // println!("cargo:rerun-if-env-changed=NCNN_INCLUDE_DIR");
+    if !lib_path.exists() {
+        panic!(
+            "ERROR: Missing lib/ directory at {}. Make sure NCNN was compiled.",
+            lib_path.display()
+        );
+    }
+
+    // Link NCNN and dependencies
+    println!("cargo:rustc-link-search=native={}", lib_path.display());
+    println!("cargo:rustc-link-lib=static=ncnn");
+    println!("cargo:rustc-link-lib=static=SPIRV");
+    println!("cargo:rustc-link-lib=static=OSDependent");
+    println!("cargo:rustc-link-lib=static=MachineIndependent");
+    println!("cargo:rustc-link-lib=static=glslang");
+    println!("cargo:rustc-link-lib=gomp");
+    // TODO: may need to change this to c++ on clang setups
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+
+    // Rebuild triggers
+    println!("cargo:rerun-if-env-changed=NCNN_DIR");
+
+    // Bindgen
     let bindings = bindgen::Builder::default()
-        .header(format!("{}/gpu.h", ncnn_include_dir.display())) // 启用gpu相关的函数；# 对cpu模式，包含gpu.h头文件，所以不做处理
-        .header(format!("{}/c_api.h", ncnn_include_dir.display())) // 通用入口
-        // .clang_arg(format!("-I{}", ncnn_include_dir.display())) // 无效
+        .header(c_api_path.to_str().unwrap())
+        .header(include_path.join("gpu.h").to_str().unwrap())
         .clang_arg("-x")
         .clang_arg("c++")
         .allowlist_type("regex")
         .allowlist_function("ncnn.*")
         .allowlist_var("NCNN.*")
         .allowlist_type("ncnn.*")
+        .wrap_unsafe_ops(true) // To avoid rust 2024 unsafe warnings
         .generate()
-        .expect("Unable to generate bindings");
+        .expect("Unable to generate bindings with bindgen");
 
     bindings
         .write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Couldn't write bindings to OUT_DIR");
 }
